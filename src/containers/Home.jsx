d419ff79dc4bb2936bf4ac2228f92dab9f7d83fa
Fernando Paredes Rios <fprios112@gmail.com>
2021-02-13 02:06:32 -0500

    subiendo archivos

---
+++ src/containers/Home.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+// import useInitialState from '../hooks/useInitialState';
+
+import '../assets/styles/App.scss';
+
+// const api = 'http://localhost:3000/initalState';
+
+const Home = ({ mylist, trends, originals }) => {
+  // const initialSate = useInitialState(api);
+
+  // console.log(initialSate);
+  return(
+  <>
+    <Search />
+    {mylist.length > 0 &&
+      <Categories title='Mi Lista'>
+        <Carousel>
+          {
+            mylist.map(item =>
+              <CarouselItem 
+              key={item.id}
+              {...item}
+              isList
+              />
+              )
+          }
+        </Carousel>
+      </Categories>
+    }
+    
+
+    <Categories title='Tendencias'>
+      <Carousel>
+        {trends.map(item =>
+          <CarouselItem key={item.id}{...item}/>
+        )}
+      </Carousel>
+    </Categories>
+
+    <Categories title='Originales de PlatziVideo'>
+      <Carousel>
+        {originals.map(item =>
+            <CarouselItem key={item.id}{...item}/>  
+          )}
+        
+      </Carousel>
+    </Categories>
+
+  </>
+  )
+};
+
+const mapStateToProps = state => {
+  return{
+    mylist: state.mylist,
+    trends: state.trends,
+    originals: state.originals,
+  }
+}
+
+export default connect(mapStateToProps, null)(Home);
